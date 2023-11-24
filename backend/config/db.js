8f0eed53f321fe160657c1f6a29ecadba0e2d055
Mohamed Ibrahim <sallamm733@gmail.com>
2023-11-25 01:23:27 +0200

    ..

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const config = require('config');
  const db = config.get('mongoURI');
 
 
-// Connect to MongoDB
+ // Connect to MongoDB
 const connectDB = async ()=>{
     try{
         await mongoose.connect(db);
