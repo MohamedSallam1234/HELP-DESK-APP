227b6f911c3427f947cf71564bc00fa57fe500db
mohamedbassem576 <mohamedbassem576@gmail.com>
2023-12-07 01:39:03 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,30 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const authRoutes = require('./routes/auth')
-const user = require("./routes/user")
-const administrator = require("./routes/administrator")
-const agent = require("../backend/routes/agent")
+const express = require("express");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/auth");
+const user = require("./routes/user");
+const administrator = require("./routes/administrator");
+const agent = require("../backend/routes/agent");
 const app = express();
 const authenticationMiddleware = require("./middleware/authanticationMiddleware");
 const cors = require("cors");
-const cookieParser = require('cookie-parser');
-
-
-
-
+const cookieParser = require("cookie-parser");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cookieParser())
-
-
-
-
+app.use(cookieParser());
 
 // Connect Database
 connectDB();
-
-app.use("/api/v1",authRoutes)
+//test
+app.use("/api/v1", authRoutes);
 app.use(authenticationMiddleware);
-app.use('/api/v1', user);
-app.use('/api/v1', administrator);
-app.use('/api/v1',agent)
+app.use("/api/v1", user);
+app.use("/api/v1", administrator);
+app.use("/api/v1", agent);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port,()=>{
-   console.log(`Server running on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
-
