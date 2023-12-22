0dca028f17ebf186b4d46429de629f2147ef7b1f
Minaehab014 <135672879+Minaehab014@users.noreply.github.com>
2023-12-22 20:32:38 +0200

    app.jsx modifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import LoginForm from './pages/Login';
+import TicketForm from './pages/Ticket';
+import SendTicket from './pages/SendTicket';
+import ViewTickets from './pages/ViewTickets';
+import UpdateInfo from './pages/updateinfo';
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
   return (
-    <>
+    <Router>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+        {/* Your navigation, header, or layout components can go here */}
 
-export default App
+        
+        {/* Define your routes */}
+        <Routes>
+          <Route exact path="/" element={<LoginForm />} />
+          <Route exact path="/Home" element={<Home />} />
+          <Route exact path="/create-ticket" element={<TicketForm />} />
+          <Route exact path="/send-ticket" element={<SendTicket />} />
+          <Route exact path="/user-profile" element={<UpdateInfo />} />
+          <Route exact path="/view-tickets" element={<ViewTickets />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
