52f487cffd08dd1556988af3b2fb5a7a1a6693d8
Minaehab014 <135672879+Minaehab014@users.noreply.github.com>
2023-12-23 22:12:29 +0200

    api to get agent specific ticket

--- backend/routes/agent.js
+++ backend/routes/agent.js
@@ -13,6 +13,7 @@ router.put("/updateticket/:ticketid",authorizationMiddleware([4]),agentControlle
 //get tickets of the agent
 router.get("/agenttickets",authorizationMiddleware([4]),agentController.showTickets)
 
-
+// get specific ticket
+router.get('/agentticket/:ticketid',authorizationMiddleware([4]),agentController.agentticket)
 
 module.exports = router;
