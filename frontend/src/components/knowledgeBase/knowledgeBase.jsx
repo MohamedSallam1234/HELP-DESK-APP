544d6960c3f85bae481b7f2be6f87c7bb3f48151
Mohamed Ibrahim <sallamm733@gmail.com>
2023-12-25 00:56:17 +0200

    knowledgeBase finshed

---
+++ frontend/src/components/knowledgeBase/knowledgeBase.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import './KnowledgeBase.css';
+
+function KnowledgeBase() {
+    const [data, setData] = useState([]);
+    const [category, setCategory] = useState("");
+
+    async function fetchData() {
+        try {
+            const response = await fetch('http://localhost:5000/api/v1/access', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                credentials: 'include',
+                body: JSON.stringify({
+                    category: category // send category in the body of the request
+                }),
+            });
+            const jsonData = await response.json();
+            setData(jsonData);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // Call fetchData when the component mounts
+    useEffect(() => {
+        fetchData().then(r => console.log(r)).catch(e => console.log(e));
+    }, []);
+
+    return (
+        <div className={"knowledgeBase"}>
+            <button onClick={fetchData}> Search Category</button>
+            <input
+                type="text"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+                placeholder="Enter category"
+            />
+            <div>
+                {data.map((item, index) => (
+                    <div key={index}>
+                        <p>Question: {item.question}</p>
+                        <p>Answer: {item.answer}</p>
+                        <p>Category: {item.category}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default KnowledgeBase;
